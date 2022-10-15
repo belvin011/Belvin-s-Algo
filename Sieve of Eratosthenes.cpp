140a1d80bec3483b8e7ce776b38c20d35c32d1b0
MANIKANTA KORIMILLI <112791483+manikanta-011@users.noreply.github.com>
2022-10-15 16:43:56 +0530

    Add files via upload

---
+++ Sieve of Eratosthenes.cpp
@@ -0,0 +1,30 @@
+//GENERATION OF PRIMES USING SIEVE OF ERATOSTHENES O(N*LOG(LOG(N)))
+#include<bits/stdc++.h>
+using namespace std;
+int N=10000000;
+bool Sieve[100000000];
+void CreateSieve(){
+    for(int i=2;i<=N;i++){
+        Sieve[i]=true;
+    }
+    for(int i=2;i*i<=N;i++){
+        if(Sieve[i]==true){
+            for(int j=i*i;j<=N;j+=i){
+                Sieve[j]=false;
+            }
+        }
+
+    }
+}
+int main(){
+    CreateSieve(); //Creates Sieve(MY BLACK BOX)
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        if(Sieve[n]==true) cout<<"Prime"<<"\n";
+        else cout<<"Not Prime"<<"\n";
+
+    }
+}
