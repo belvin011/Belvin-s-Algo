67eeba6d932309f14cd7fa300645a4583cf7188d
MANIKANTA KORIMILLI <112791483+belvin011@users.noreply.github.com>
2022-12-02 18:28:48 +0530

    Add files via upload

---
+++ ModularArithemetic.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+const int M=1e9+7;
+long long Mod(long long x){
+    return ((x%M + M)%M); }
+long long Add(long long a, long long b){
+    return Mod(Mod(a)+Mod(b)); }
+long long Mul(long long a, long long b){
+    return Mod(Mod(a)*Mod(b)); }
+    
+int main(){
+
+    cout<<Add(10,20)<<"\n"; 
+      
+}
+
